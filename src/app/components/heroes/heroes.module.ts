1b639a382feed88705f8b441f34616cc08d11de4
lodela <lodela.n@gmail.com>
2023-05-25 09:20:30 -0600

    Feat: add new module
    This commit adds a new module to the application.
    
    ## Details
    
    * Added a new module called `dbz`.
    * This module includes all its empty directories.
    
    ## Related tickets
    
    * #1234 - Add new module dbz meanin: dragon ball z

--- src/app/components/heroes/heroes.module.ts
+++ src/app/components/heroes/heroes.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { HeroComponent } from './hero/hero.component';
 import { ListComponent } from './list/list.component';
 @NgModule({
