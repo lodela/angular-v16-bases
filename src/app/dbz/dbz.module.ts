69d76ab84e0a5a8bfd98415f21abba6fa8e43f46
lodela <lodela.n@gmail.com>
2023-05-25 10:07:49 -0600

    Feat(dbz): Add separate components for add-character-form and character-list
    Adds separate components for the add-character-form and character-list.
    
    Closes #125

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainPageComponent } from './pages/main-page.component';
+import { CharacterListComponent } from './components/character-list/character-list.component';
+import { AddCharacterFormComponent } from './components/add-character-form/add-character-form.component';
 
 @NgModule({
-  declarations: [MainPageComponent],
+  declarations: [MainPageComponent, CharacterListComponent, AddCharacterFormComponent],
   imports: [CommonModule],
   exports: [MainPageComponent],
 })
